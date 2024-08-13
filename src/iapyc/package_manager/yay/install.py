7b5ba7b3c859f3c2ab3e2bbe3d3146b8043592ff
CYTMWIA <58131156+CYTMWIA@users.noreply.github.com>
2024-08-13 23:19:43 +0800

    [yay] support http proxy

--- src/iapyc/package_manager/yay/install.py
+++ src/iapyc/package_manager/yay/install.py
@@ -11,6 +11,12 @@ def install(host: Host, packages: str | list[str]):
         packages = [packages]
     pkgs_s = " ".join(packages)
 
+    proxy = host.get_var("yay_http_proxy")
+    if proxy is None:
+        env = ""
+    else:
+        env = f"https_proxy={proxy} http_proxy={proxy}"
+
     sudo_pacman_disable_password()  # 预防可能的输密码环节
-    host.run(f"yay -Sy --noconfirm --needed {pkgs_s}", hide=False)
+    host.run(f"{env} yay -Sy --noconfirm --needed {pkgs_s}", hide=False)
     sudo_pacman_enable_password()
